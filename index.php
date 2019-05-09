bca5b875715baf86aaa3a24237e5e66f7206ffb3
amina <aminaghariani16@gmail.com>
2019-05-09 14:44:12 +0100

    sidebar

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
    <?php get_header();?>
-       
-
-          <?php get_template_part( 'content', get_post_format() ); ?>
- 
-
-    
-    
+ <div class="row">  
+ <div class="col-sm-8 blog-main"> 
+ <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>   <!-- contents of the loop --> <?php endwhile;?> <nav>
+ <ul class="pager">
+<li><?php next_posts_link( 'Previous' ); ?></li>
+<li><?php previous_posts_link( 'Next' ); ?></li>
+ </ul>
+</nav> <?php endif; ?>
+     <?php get_template_part( 'content', get_post_format() ); ?> 
+	   </div> <!-- /.blog-main -->   <?php get_sidebar(); ?>
+     </div>
    <?php get_footer();?>
            
