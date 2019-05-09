69dd55fb226a1ebbeaaa81d97fdf14c8e747e004
amina <aminaghariani16@gmail.com>
2019-05-09 14:14:14 +0100

    division

--- header.php
+++ header.php
@@ -73,3 +73,19 @@ rel="stylesheet">
                 </nav>
             </header>
             <!-- End Header Area -->
+              <!-- Start top-section Area -->
+            <section class="top-section-area section-gap">
+                <div class="container">
+                    <div class="row justify-content-between align-items-center d-flex">
+                        <div class="col-lg-8 top-left">
+                            <h1 class="text-white mb-20">Fashion News</h1>
+                            <ul>
+                                <li><a href="index.html">Home</a><span class="lnr lnr-arrow-right"></span></li>
+                                <li><a href="category.html">Category</a><span class="lnr lnr-arrow-right"></span></li>
+                                <li><a href="single.html">Fashion</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>  
+            </section>
+            <!-- End top-section Area -->
