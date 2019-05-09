35fca8e3dadc13368023bf8ef392ee2b09ef6073
amalaouichi <48316234+amalaouichi@users.noreply.github.com>
2019-05-09 17:05:53 +0200

    correction index

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
    <?php get_header();?>
  <div class="row">  
  <div class="col-sm-8 blog-main"> 
- <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>   <!-- contents of the loop --> <?php endwhile;?> <nav>
+ <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>   <!-- contents of the loop --> 
+      <?php get_template_part( 'content', get_post_format() ); ?> 
+	  <?php endwhile;?>  <nav>
  <ul class="pager">
 <li><?php next_posts_link( 'Previous' ); ?></li>
 <li><?php previous_posts_link( 'Next' ); ?></li>
  </ul>
-</nav> <?php endif; ?>
-     <?php get_template_part( 'content', get_post_format() ); ?> 
+</nav>
+ <?php endif;?>
+
+	 
 	   </div> <!-- /.blog-main -->   <?php get_sidebar(); ?>
      </div>
    <?php get_footer();?>
