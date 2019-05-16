21b9bee16c4796513e60c543658b2e2181f72f16
amina <aminaghariani16@gmail.com>
2019-05-16 14:40:17 +0100

    réglage de content

--- content.php
+++ content.php
@@ -2,38 +2,16 @@
   <h2 class="blog-post-title">
       <a href="<?php the_permalink(); ?>">
           <?php the_title(); ?></a></h2>
-     <img class="img-fluid" src="<?php bloginfo('template_directory');?>/img/35848.jpg" alt="" height="300" width="300">
-<a href="<?php comments_link(); ?>"> 
-<?php printf( _nx( 'One Comment','%1$s Comments', get_comments_number(), 'comments title', 'textdomain' ),
-number_format_i18n( get_comments_number() ) ); ?> </a>
-    <?php the_content(); ?>
-        <a href="<?php the_permalink(); ?>">
-          <?php the_title(); ?></a>
-     <img class="img-fluid" src="<?php bloginfo('template_directory');?>/img/32180.jpg" alt="" height="300" width="300">
-    <a href="<?php comments_link(); ?>"> 
-<?php printf( _nx( 'One Comment','%1$s Comments', get_comments_number(), 'comments title', 'textdomain' ),
-number_format_i18n( get_comments_number() ) ); ?> </a>
     
-     <a href="<?php the_permalink(); ?>">
-          <?php the_title(); ?></a>
-     <img class="img-fluid" src="<?php bloginfo('template_directory');?>/img/24.jpg" alt="" height="300" width="300">
-    <a href="<?php comments_link(); ?>"> 
-<?php printf( _nx( 'One Comment','%1$s Comments', get_comments_number(), 'comments title', 'textdomain' ),
-number_format_i18n( get_comments_number() ) ); ?> </a>
+    <?php if ( has_post_thumbnail() ) {?>
     
-<p class="blog-post-meta">
-     <?php the_date(); ?> par <a href="#"><?php the_author(); ?></a> 
-      <?php the_content(); ?>
-    <a href="<?php comments_link(); ?>"> 
-<?php printf( _nx( 'One Comment','%1$s Comments', get_comments_number(), 'comments title', 'textdomain' ),
-number_format_i18n( get_comments_number() ) ); ?> </a></p>
-  <?php the_excerpt(); ?> </div><!-- /.blog-post --> 
-<?php if ( has_post_thumbnail() ) {?>
 <div class="row">
 <div class="col-md-4"> <?php the_post_thumbnail('thumbnail'); ?>
 </div>
-<div class="col-md-6"> <?php the_excerpt(); ?> </div>
+<div class="col-md-6"> <?php the_content(); ?> </div>
 </div>
 <?php } else { ?>
-<?php the_excerpt(); ?> <?php } ?>
+<?php the_content(); ?> <?php } ?>
+
+   </div><!-- /.blog-post --> 
 
